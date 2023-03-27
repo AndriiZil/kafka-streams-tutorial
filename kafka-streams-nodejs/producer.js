901144db6f92187e22b1e2f9cfc2a2a2f427cc9d
Andrii Zilnyk <andrii.zilnyk@intellias.com>
2023-03-27 23:37:53 +0300

    added kafka node streams

---
+++ kafka-streams-nodejs/producer.js
@@ -0,0 +1,27 @@
+const { Kafka, Partitioners } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092'],
+});
+
+const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner });
+
+(async () => {
+    try {
+        await producer.connect();
+
+        await producer.send({
+            topic: 'test-topic',
+            messages: [
+                { value: 'Hello KafkaJS user!' },
+            ],
+        });
+
+        await producer.disconnect()
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+
---
+++ kafka-streams-nodejs/streams.js
@@ -0,0 +1,16 @@
+const { KafkaStreams } = require('kafka-streams');
+const config = require('./options.json');
+
+const kafkaStreams = new KafkaStreams(config);
+
+const kafkaTopicName = 'test-topic';
+const stream = kafkaStreams.getKStream(kafkaTopicName);
+
+stream.forEach(message => console.log(message.value.toString()));
+stream.mapJSONConvenience().forEach(message => console.log(message));
+
+stream.start().then(() => {
+    console.log("stream started, as kafka consumer is ready.");
+}, error => {
+    console.log("streamed failed to start: " + error);
+});
